9f63f86bd9684dce1bb2fe2c3642d62528c39a86
Altusha4 <240782@astanait.edu.kz>
2025-11-12 21:33:08 +0600

    Implement TripBuilder and TripDirector for building trips

--- src/builder/TripDirector.java
+++ src/builder/TripDirector.java
@@ -1,4 +1,30 @@
 package builder;
+import entities.*;
 
 public class TripDirector {
+    private TripBuilder builder;
+
+    public TripDirector(TripBuilder builder) {
+        this.builder = builder;
+    }
+    public Trip createStandardTrip() {
+        Route route = new Route("Astana", "Rome", "scenic");
+        Flight flight = new Flight("Astana", "Rome", "Air Astana", 500.0);
+        Hotel hotel = new Hotel("Rome", "Roma Hotel", 3, 120.0);
+
+        return builder.setRoute(route)
+                      .setFlight(flight)
+                      .setHotel(hotel)
+                      .build();
+    }
+    public Trip createCustomTrip(String from, String to) {
+        Route route = new Route(from, to, "scenic");
+        Flight flight = new Flight(from, to, "Air Astana", 600.0);
+        Hotel hotel = new Hotel(to, "Hotel Example", 4, 150.0);
+
+        return builder.setRoute(route)
+                .setFlight(flight)
+                .setHotel(hotel)
+                .build();
+    }
 }

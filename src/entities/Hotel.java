b9c39da9b08ed810324ec70b6d6e9cc73a986747
Altusha4 <240782@astanait.edu.kz>
2025-11-12 21:09:23 +0600

    Create Hotel class: represents a hotel with city, name, nights, and price per night

---
+++ src/entities/Hotel.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Hotel {
+    private String city;
+    private String name;
+    private int nights;
+    private double pricePerNight;
+
+    public Hotel(String city, String name, int nights, double pricePerNight) {
+        this.city = city;
+        this.name = name;
+        this.nights = nights;
+        this.pricePerNight = pricePerNight;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getNights() {
+        return nights;
+    }
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+    public double totalPrice() {
+        return nights * pricePerNight;
+    }
+}

bf9e77e51cd3b0dfcdca6ac833b297f944dd1675
Altusha4 <240782@astanait.edu.kz>
2025-11-12 21:07:01 +0600

    Create Route class: represents a route with departure, destination, and style (fastest/scenic)

---
+++ src/entities/Route.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Route {
+    private String from;
+    private String to;
+    private String style;
+
+    public Route(String from, String to, String style) {
+        this.from = from;
+        this.to = to;
+        this.style = style;
+    }
+    public String getFrom() {
+        return from;
+    }
+    public String getTo() {
+        return to;
+    }
+    public String getStyle() {
+        return style;
+    }
+}

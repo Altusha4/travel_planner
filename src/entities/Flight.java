5f801f128a56a8763a0c55898a957ca63f69e00d
Altusha4 <240782@astanait.edu.kz>
2025-11-12 21:08:18 +0600

    Create Flight class: represents a flight with details like from, to, airline, and price

---
+++ src/entities/Flight.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Flight {
+    private String from;
+    private String to;
+    private String airline;
+    private double price;
+
+    public Flight(String from, String to, String airline, double price) {
+        this.from = from;
+        this.to = to;
+        this.airline = airline;
+        this.price = price;
+    }
+    public String getFrom() {
+        return from;
+    }
+    public String getTo() {
+        return to;
+    }
+    public String getAirline() {
+        return airline;
+    }
+    public double getPrice() {
+        return price;
+    }
+}

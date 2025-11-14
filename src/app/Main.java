4d0ab4dd88edd1cc35ac25cfac9684f14a0f5614
Altusha4 <240782@astanait.edu.kz>
2025-11-14 15:55:04 +0600

    main with adapter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,9 @@
-package App;
+package app;
+import service.TripService;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        TripService tripService = new TripService();
+        tripService.createTrip();
+    }
 }

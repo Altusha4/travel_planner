b1ce92fdd5435e24ed11c34760c765099e9a5aa2
Altusha4 <240782@astanait.edu.kz>
2025-11-14 15:52:11 +0600

    feat: add HotelAdapter with CSV file reading

--- src/adapter/HotelAdapter.java
+++ src/adapter/HotelAdapter.java
@@ -1,4 +1,29 @@
 package adapter;
+import entities.Hotel;
+import java.util.Scanner;
+import java.io.File;
 
 public class HotelAdapter {
+    public Hotel getHotel(String city) throws Exception {
+
+        File file = new File("src/data/hotels.csv");
+        Scanner scanner = new Scanner(file);
+
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] data = line.split(",");
+
+            if (data[0].equalsIgnoreCase(city)) {
+                scanner.close();
+                return new Hotel(data[0], data[1],
+                        Integer.parseInt(data[2]),
+                        Double.parseDouble(data[3]));
+            }
+        }
+        scanner.close();
+        return null;
+    }
 }

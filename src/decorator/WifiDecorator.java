2f94f81777d845c18692b4faf6a6dfd4ba741784
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:40:23 +0600

    implement WifiDecorator for internet service

--- src/decorator/WifiDecorator.java
+++ src/decorator/WifiDecorator.java
@@ -1,4 +1,18 @@
 package decorator;
 
-public class WifiDecorator {
+public class WifiDecorator extends TripDecorator {
+
+    public WifiDecorator(TripComponent trip) {
+        super(trip);
+    }
+
+    @Override
+    public double getTotalCost() {
+        return super.getTotalCost() + 10;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " + WiFi";
+    }
 }

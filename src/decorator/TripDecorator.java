16adf89d6bf277586e68099713626aacd867bb81
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:38:38 +0600

    implement base TripDecorator class for Decorator pattern

--- src/decorator/TripDecorator.java
+++ src/decorator/TripDecorator.java
@@ -1,4 +1,20 @@
 package decorator;
 
-public class TripDecorator {
+public abstract class TripDecorator implements TripComponent {
+
+    protected TripComponent trip;
+
+    public TripDecorator(TripComponent trip) {
+        this.trip = trip;
+    }
+
+    @Override
+    public double getTotalCost() {
+        return trip.getTotalCost();
+    }
+
+    @Override
+    public String getDescription() {
+        return trip.getDescription();
+    }
 }

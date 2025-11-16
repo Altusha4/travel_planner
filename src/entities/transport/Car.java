9a9086755c30d765cf8dea85a5a1acade2f547ea
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:33:56 +0600

    implements Car transport class

--- src/entities/transport/Car.java
+++ src/entities/transport/Car.java
@@ -1,4 +1,12 @@
 package entities.transport;
 
-public class Car {
+public class Car implements Transport {
+    @Override
+    public String getType() {
+        return "Car";
+    }
+    @Override
+    public int getSpeed() {
+        return 85;
+    }
 }

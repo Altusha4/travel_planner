d0e0a81f0c4a45f26de0dbab71ae420aef3e6f1e
Altusha4 <240782@astanait.edu.kz>
2025-11-16 23:11:12 +0600

    add CsvHotelService as Adapter Adaptee

--- src/adapter/CsvHotelService.java
+++ src/adapter/CsvHotelService.java
@@ -1,4 +1,22 @@
 package adapter;
+import java.util.Scanner;
+import java.io.File;
 
 public class CsvHotelService {
+    public String[] findHotelData(String city) throws Exception {
+        File file = new File("src/data/hotels.csv");
+        Scanner scanner = new Scanner(file);
+
+        if (scanner.hasNextLine()) scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] data = line.split(",");
+            if (data[0].equalsIgnoreCase(city)) {
+                scanner.close();
+                return data;
+            }
+        }
+        scanner.close();
+        return null;
+    }
 }

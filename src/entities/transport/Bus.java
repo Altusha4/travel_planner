6ae32db5e68ce09d1f498b1ee6ddba1a7db74d3a
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:34:33 +0600

    implements Bus transport class

--- src/entities/transport/Bus.java
+++ src/entities/transport/Bus.java
@@ -1,4 +1,12 @@
 package entities.transport;
 
-public class Bus {
+public class Bus implements Transport {
+    @Override
+    public String getType() {
+        return "Bus";
+    }
+    @Override
+    public int getSpeed() {
+        return 75;
+    }
 }

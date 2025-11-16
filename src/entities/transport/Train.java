d3c70d6c1eccd8204d3c4f02da1731e1768bf0e5
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:32:26 +0600

    implement Train transport class

--- src/entities/transport/Train.java
+++ src/entities/transport/Train.java
@@ -1,4 +1,12 @@
 package entities.transport;
 
-public class Train {
+public class Train implements Transport {
+    @Override
+    public String getType() {
+        return "Train";
+    }
+    @Override
+    public int getSpeed() {
+        return 250;
+    }
 }

05859e0f3cc1437cf47784beb71e4ca15c41c8bd
Altusha4 <240782@astanait.edu.kz>
2025-11-16 23:10:10 +0600

    add CsvFlightService as Adapter Adaptee

--- src/adapter/CsvFlightService.java
+++ src/adapter/CsvFlightService.java
@@ -1,4 +1,22 @@
 package adapter;
+import java.util.Scanner;
+import java.io.File;
 
 public class CsvFlightService {
+    public String[] findFlightData(String from, String to) throws Exception {
+        File file = new File("src/data/flights.csv");
+        Scanner scanner = new Scanner(file);
+
+        if (scanner.hasNextLine()) scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] data = line.split(",");
+            if (data[0].equalsIgnoreCase(from) && data[1].equalsIgnoreCase(to)) {
+                scanner.close();
+                return data;
+            }
+        }
+        scanner.close();
+        return null;
+    }
 }

2fe7368296253b5b805551fd36d0d2cecf38d5cd
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:33:11 +0600

    implement Plane transport class

--- src/entities/transport/Plane.java
+++ src/entities/transport/Plane.java
@@ -1,4 +1,12 @@
 package entities.transport;
 
-public class Plane {
+public class Plane implements Transport {
+    @Override
+    public String getType() {
+        return "Plane";
+    }
+    @Override
+    public int getSpeed() {
+        return 900;
+    }
 }

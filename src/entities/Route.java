573a7499cd88187e127d3fb025d19a00e0064798
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:36:50 +0600

    extend Route class with Bridge pattern support

--- src/entities/Route.java
+++ src/entities/Route.java
@@ -1,17 +1,31 @@
 package entities;
 
+import entities.transport.Transport;
+
 public class Route {
     private String from;
     private String to;
     private String style;
+    private Transport transport;
 
     public Route(String from, String to, String style) {
         this.from = from;
         this.to = to;
         this.style = style;
+        this.transport = null;
     }
 
+    public void setTransport(Transport transport) {
+        this.transport = transport;
+    }
+
+    @Override
     public String toString() {
-        return from + " -> " + to + " (" + style + ")";
+        String transportInfo = "";
+        if (transport != null) {
+            transportInfo = " | transport: " + transport.getType()
+                    + " (" + transport.getSpeed() + " km/h)";
+        }
+        return from + " -> " + to + " (" + style + ")" + transportInfo;
     }
 }

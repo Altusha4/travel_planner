f951608f7563cb923c2caa99c0f729942df792d1
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:39:57 +0600

    implement InsuranceDecorator for travel insurance

--- src/decorator/InsuranceDecorator.java
+++ src/decorator/InsuranceDecorator.java
@@ -1,4 +1,18 @@
 package decorator;
 
-public class InsuranceDecorator {
+public class InsuranceDecorator extends TripDecorator {
+
+    public InsuranceDecorator(TripComponent trip) {
+        super(trip);
+    }
+
+    @Override
+    public double getTotalCost() {
+        return super.getTotalCost() + 45;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " + Insurance";
+    }
 }

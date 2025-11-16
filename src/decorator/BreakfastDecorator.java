17cf3223e4ca03a32e81e2ccfdfcb19b4d3929b3
Altusha4 <240782@astanait.edu.kz>
2025-11-16 19:39:26 +0600

    implement BreakfastDecorator for additional services

--- src/decorator/BreakfastDecorator.java
+++ src/decorator/BreakfastDecorator.java
@@ -1,4 +1,18 @@
 package decorator;
 
-public class BreakfastDecorator {
+public class BreakfastDecorator extends TripDecorator {
+
+    public BreakfastDecorator(TripComponent trip) {
+        super(trip);
+    }
+
+    @Override
+    public double getTotalCost() {
+        return super.getTotalCost() + 20;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " + Breakfast";
+    }
 }
